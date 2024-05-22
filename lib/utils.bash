11862fd521ccfc1d9662a6771d385759f4ab887f
HaloGithub <28960001+HaloGithub@users.noreply.github.com>
2024-05-23 01:52:22 +0800

    First implementation. (#6)

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for novops.
 GH_REPO="https://github.com/PierreBeucher/novops"
 TOOL_NAME="novops"
 TOOL_TEST="novops --version"
@@ -31,8 +30,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if novops has other means of determining installable versions.
 	list_github_tags
 }
 
@@ -40,9 +37,18 @@ download_release() {
 	local version filename url
 	version="$1"
 	filename="$2"
+	platform=$(uname | tr '[:upper:]' '[:lower:]')
+	arch=$(uname -m)
 
-	# TODO: Adapt the release URL convention for novops
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	if [[ "$platform" == 'darwin' ]]; then
+		platform='macos'
+	fi
+
+	if [[ "$arch" == 'arm64' ]]; then
+		arch='aarch64'
+	fi
+
+	url="$GH_REPO/releases/download/v${version}/novops_${platform}_${arch}.zip"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -61,7 +67,6 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert novops executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
